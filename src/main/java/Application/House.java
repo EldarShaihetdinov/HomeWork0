f8210ae0601b21dac2a6766c6de6481a2073bc0c
Aldar <ShaihetdinovAld@gmail.com>
2021-09-30 17:12:37 +0400

    dev ver 0.4

--- src/main/java/Application/House.java
+++ src/main/java/Application/House.java
@@ -1,6 +1,34 @@
 package Application;
 
 public class House {
+    final private int STAGE = 3;
+    private String OwnerName;
+    private String Colour;
+
+    public House(String OwnerName, String colour) {
+
+        this.OwnerName = OwnerName;
+        this.Colour = colour;
+    }
 
 
+    public String getOwnerName(){
+        return OwnerName;
+    }
+    public void setOwnerName(String name){
+        this.OwnerName = name;
+    }
+
+
+    public int getSTAGE() {
+        return STAGE;
+    }
+
+    public String getColour() {
+        return Colour;
+    }
+
+    public void setColour(String colour) {
+        this.Colour = colour;
+    }
 }
